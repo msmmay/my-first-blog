ff23bb7df666939602694da1ab641f8f5a924d36
Mnena <mnena16@yahoo.com>
2019-10-25 21:13:14 +0100

    Corrected errors in code

--- blog/views.py
+++ blog/views.py
@@ -39,6 +39,7 @@ def post_edit(request, pk):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', ph=post.pk)
-    else: form = PostForm(instance=post)
+            return redirect('post_detail', pk=post.pk)
+    else: 
+        form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
